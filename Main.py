411f11c4837cb166c84783a6034e3d4a7bb192cf
Arif Nur Ramadhan <91766087+arifnrrmdn@users.noreply.github.com>
2022-11-06 18:58:45 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,13 @@
+from asyncore import write
+import streamlit as st
+
+st.write("""
+# Kamu Pilih Dia atau Aku!
+""")
+
+if st.button("1. Aku"):
+    st.subheader("Aku akan selalu ada buat kamu")
+
+
+if st.button("2. Dia"):
+    st.subheader("Yaudah deh gakpapa :)")
